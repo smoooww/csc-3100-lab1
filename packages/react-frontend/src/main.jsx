e73c5a6112c7a03ca7bd1fd1be52c576e008ce77
Veronika Lebed <veronikalebed@Veronikas-MacBook-Air.local>
2026-08-30 11:40:00 -0700

    Step 6 React First App

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+import MyApp from "./MyApp";
+// Create the container
+const container = document.getElementById("root");
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create a rott
+const root = ReactDOMClient.createRoot(container);
+
+//Initial render: Render an element to the Root
+root.render(<MyApp />);
