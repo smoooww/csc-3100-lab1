0c0f1f8c0186ead30cd751f3f579561e32249a22
Veronika Lebed <lvs97485@email.com>
2026-09-07 23:43:02 -0700

    POST add users

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,102 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = {
+	users_list: [
+		{
+		 id: "xyz789",
+		 name: "Charlie",
+		 job: "Janitor"
+		},
+		{
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    },
+    {
+  "id": "qwe123",
+  "job": "Zookeeper",
+  "name": "Cindy"
+  }
+	]
+};
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+const findUserById = (id) => users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post ("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+const deleteUserByID = (id) => {
+  users["users_list"] = users["users_list"].filter(user => user["id"] !== id
+  );
+};
+app.delete ("/users/:id", (req, res) => {
+  const userToDelete = req.params["id"];
+  deleteUserByID(userToDelete);
+  res.send();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = {users_list: result};
+    res.send(result);
+  } else {
+	res.send(users);
+  };
+});
+app.listen(port, () => {
+	console.log(
+		`Example app listening at http://localhost:${port}`
+	);
+});
+
+
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -13,7 +13,34 @@ function MyApp() {
         }
 
         function updateList(person) {
-            setCharacters([...characters, person]);
+            postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+
+        function fetchUsers() {
+            const promise = fetch("http://localhost:8000/users");
+            return promise;
+        }
+
+        useEffect(() => {
+            fetchUsers()
+                .then((res) => res.json())
+                .then((json) => setCharacters(json["users_list"]))
+                .catch((error) => {console.log(error);});
+        }, []);
+
+        function postUser(person) {
+            const promise = fetch("http://localhost:8000/users", {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json", 
+                },
+                body: JSON.stringify(person),
+            });
+            return promise;
         }
 
         return (
